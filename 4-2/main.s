72d970642371ed86fd3fd216c0ff1b24df987a23
Alaska-666 <36411575+Alaska-666@users.noreply.github.com>
2019-05-19 12:06:58 +0300

    Create main.s

---
+++ 4-2/main.s
@@ -0,0 +1,26 @@
+﻿	.text
+	.global check_int
+check_int:
+//проверка на ноль
+	cmp r0, #0
+	beq return
+//выполняем сдвиг вправо, пока последняя цифра не 1
+loop:
+	mov r1, #1
+	and r1, r0, r1
+	cmp r1, #1
+	beq check
+	lsr r0, #1
+	b loop
+check:
+	mov r1, #0x000000FF
+	orr r1, r1, #0x0000FF00
+	orr r1, r1, #0x00FF0000
+//r1 самое большое число, которое можно запихнуть в мантиссу
+	cmp r0, r1
+	bls return
+	mov r0, #0
+	bx lr
+return:
+	mov r0, #1
+	bx lr
