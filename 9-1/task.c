24267f43572526c0cdf755c1ac2e1edfc8b612ae
Alaska-666 <36411575+Alaska-666@users.noreply.github.com>
2019-05-20 10:57:26 +0300

    Create task.c

---
+++ 9-1/task.c
@@ -0,0 +1,30 @@
+﻿#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int args, char* argv[]) {
+	int file = open(argv[1], O_RDONLY);
+	//проверка на несуществующий файл
+	if (file < 0) {
+		return 1;
+	}
+	int value = 0;
+	uint32_t next_pointer = 0;
+	//проверка на пустой существующий
+	if (read(file, &value, sizeof(value)) <= 0) {
+		close(file);
+		return 0;
+	}
+	read(file, &next_pointer, sizeof(next_pointer));
+	printf("%d\n", value);
+	while (next_pointer > 0) {
+		lseek(file, next_pointer, SEEK_SET);
+		read(file, &value, sizeof(value));
+		read(file, &next_pointer, sizeof(next_pointer));
+		printf("%d\n", value);
+	}
+	close(file);
+	return 0;
+}
